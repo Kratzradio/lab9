5f1aac8373154d3e2b82e559c1d9713283414d85
Kratzradio <mkersten@pc08.pool.tp1.uni-duesseldorf.de>
2016-01-06 17:34:40 +0100

    upwind und FTCS

--- upwind.cxx
+++ upwind.cxx
@@ -10,17 +10,18 @@ void writeToFile(const double* const u, const string s, const double dx,
                  const double xmin, const int N);
 void initialize(double* const u, const double dx, const double xmin,
                 const int N);
+void step(double* u0, double* u1, const double dx, const double dt, const double V, const int N);
 //---------------------------------------
 int main(){
 
-  const double tEnd = ;
-  const double V = ;
+  const double tEnd = 5.0;
+  const double V = 1.0;
 
-  const int N  = ;
-  const double xmin = -10;
-  const double xmax =  10;
-  const double dx = (xmax-xmin)/(N-1);
-  double dt = ;
+  const int N  = 256;
+  const double xmin = -10.0;
+  const double xmax =  10.0;
+  const double dx = (xmax-xmin)/(N-1.0);
+  double dt = 0.1*dx/V;
   const int Na = 10; // Number of output files up to tEnd
   const int Nk = int(tEnd/Na/dt);
 
@@ -39,10 +40,13 @@ int main(){
    for(int j=0; j<Nk; j++){
 
       // Put call to step function here
-
+	step(u0, u1, dx, dt, V, N);
       // swap arrays u0 <-> u1,
       // however do not copy values, be more clever ;)
-   }
+	h=u0 ; u0=u1; u1=h;
+     
+	
+  }
    strm.str("");
    strm << "u_" << i;
    writeToFile(u0, strm.str(), dx, xmin, N);
@@ -78,4 +82,12 @@ void writeToFile(const double* const u, const string s, const double dx,
      out << x << "\t" << u[i] << endl;
    }
    out.close();
+}
+
+void step(double* u0, double* u1, const double dx, const double dt, const double V, const int N)
+{
+    u1[0] = 0;
+  //for(int i=1; i<N; i++){u1[i] = -V * dt/dx * (u0[i] - u0[i-1]) + u0[i];}
+  for(int i=1; i<N-1; i++){u1[i] = -V * dt * (u0[i+1] - u0[i-1])/(2.0*dx) + u0[i];} 
+    u1[N-1] = 0;
 }
